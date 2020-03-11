a95c60ed464c9d0ed12b610bd5bc86c1c379a3ff
wdwang <wedonsjtu@sjtu.edu.cn>
2020-03-11 22:53:46 +0800

    Add 309.cpp

--- 309. Best Time to Buy and Sell Stock with Cooldown.cpp
+++ 309. Best Time to Buy and Sell Stock with Cooldown.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         if (prices.size() < 2) return 0;
-        int cash_sell_yesterday = -prices[0], cash_not_sell_yesterday = 0, hold = -prices[0];
+        int cash_sell_yesterday = 0, cash_not_sell_yesterday = 0, hold = -prices[0];
         int max_cash;
         for (int i=1; i < prices.size(); ++i) {
             max_cash = max(cash_sell_yesterday, cash_not_sell_yesterday);
