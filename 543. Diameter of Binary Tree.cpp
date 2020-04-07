834d0fb14ba1c52dcb4317b6057ab0b65f860529
wdwang <wedonsjtu@outlook.com>
2020-04-07 23:28:36 +0800

    Add 543.cpp

---
+++ 543. Diameter of Binary Tree.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    ~TreeNode() {
+        if (left) delete left;
+        if (right) delete right;
+    }
+};
+
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        int diameter = 0;
+        depth(root, diameter);
+        return diameter;
+    }
+    
+    int depth(TreeNode* root, int &diameter) {
+        if (!root) return 0;
+        int left = depth(root->left, diameter);
+        int right = depth(root->right, diameter);
+        diameter = max(diameter, left + right);
+        return max(left, right) + 1;
+    }
+};
+
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+int main() {
+    string line = "[3,9,20,null,null,15,7]";
+    TreeNode* root = stringToTreeNode(line);
+    int ret = Solution().diameterOfBinaryTree(root);
+    string out = to_string(ret);
+    cout << out << endl;
+    return 0;
+}
